7f10f1492e4b563dcb52963ad276a1c36bb93261
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-05 14:16:22 +0300

    search and chats components' style are done

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,7 +2,43 @@
 
 const Chats = () => {
     return (
-        <div>Chats</div>
+        <div className="chats">
+            <div className="userChat">
+                <img src="https://images.pexels.com/photos/1326946/pexels-photo-1326946.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <div className="userChatInfo">
+                    <span>Jennifer</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="userChat">
+                <img src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <div className="userChatInfo">
+                    <span>Jennifer</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="userChat">
+                <img src="https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <div className="userChatInfo">
+                    <span>Jennifer</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="userChat">
+                <img src="https://images.pexels.com/photos/567459/pexels-photo-567459.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <div className="userChatInfo">
+                    <span>Jennifer</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="userChat">
+                <img src="https://images.pexels.com/photos/948873/pexels-photo-948873.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <div className="userChatInfo">
+                    <span>Jennifer</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@
 
 const Search = () => {
     return (
-        <div>Search</div>
+        <div className="search">
+            <div className="searchForm">
+                <input type="text" placeholder="Find a user" />
+            </div>
+            <div className="userChat">
+                <img src="https://images.pexels.com/photos/1130625/pexels-photo-1130625.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <div className="userChatInfo">
+                    <span>Jennifer</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
