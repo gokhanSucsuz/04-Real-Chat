36767f4eba1b18e066afaa42fdc7d5231e132bdf
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-05 16:16:53 +0300

    firebase login is doing...

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyArY6I-gZNMDkAW976wiVsyLsyQdiJjnUQ",
@@ -12,4 +13,5 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,58 @@
 import { MdOutlineAddPhotoAlternate } from "react-icons/md"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, storage } from "../firebase";
+import { useState } from "react";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
 
 export const Register = () => {
+
+    const [err, setErr] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+
+
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password)
+
+            const storageRef = ref(storage, displayName);
+
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on('state_changed',
+                (snapshot) => {
+
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                    }
+                },
+                (error) => {
+
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        console.log('File available at', downloadURL);
+                    });
+                }
+            );
+
+        } catch (error) {
+
+        }
+
+    }
     return (
         <div className="formContainer">
             <div className="formWrapper">
@@ -8,7 +60,7 @@ export const Register = () => {
                     Real Chat
                 </div>
                 <div className="title">Register</div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="display name" />
                     <input type="email" placeholder="email" />
                     <input type="password" placeholder="password" />
@@ -17,11 +69,15 @@ export const Register = () => {
                         <MdOutlineAddPhotoAlternate className="imageIcon" />
                         Add an avatar
                     </label>
+
+                    <button>
+                        Sign Up
+                    </button>
+                    {
+                        err && <span>Something went wrong!</span>
+                    }
+                    <p>You do have an account? Login</p>
                 </form>
-                <button>
-                    Sign Up
-                </button>
-                <p>You do have an account? Login</p>
             </div>
         </div>
     )
