8eed539f044fb561f94bb6059ca67a08a4d4e57c
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-11 11:22:04 +0300

    chats is finished

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,23 +1,38 @@
 /* eslint-disable react/prop-types */
 
-import { useContext } from "react"
+import { useContext, useEffect, useRef } from "react"
 import { AuthContext } from "../context/AuthContext"
 import { ChatContext } from "../context/ChatContext"
 
 const Message = ({ message }) => {
     const { currentUser } = useContext(AuthContext)
     const { data } = useContext(ChatContext)
-    return
-    // <div className="message owner">
-    //     <div className="messageInfo">
-    //         <img src={currentUser.photoURL} alt="" />
-    //         <span>Just Now</span>
-    //     </div>
-    //     <div className="messageContent">
-    //         <p>{data.message}</p>
-    //         <img src="https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-    //     </div>
-    // </div>
+
+    const convertDate = (timestamp) => {
+        let dateInMillis = timestamp * 1000
+        let date = new Date(dateInMillis)
+        let myTime = date.toLocaleTimeString()
+        return myTime
+    }
+
+    const ref = useRef()
+
+    useEffect(() => {
+        ref.current?.scrollIntoView({ behavior: "smooth" })
+    }, [message])
+
+    return (<div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
+        <div className="messageInfo">
+            <img src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
+            <span>{convertDate(message.date)}</span>
+        </div>
+        <div className="messageContent">
+            <p>{message.text}
+            </p>
+            {message.img && <img src={message.img} alt="" />}
+
+        </div>
+    </div>)
 
 }
 
